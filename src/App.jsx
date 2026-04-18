40dcfcdfb5cdfc126acfbd5d5ea6f710a11ccee9
alam3131 <lamanthony678@gmail.com>
2026-04-18 14:24:49 -0500

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css'
-import Home from './pages/Home';
-import Auth from './pages/Auth';
-import Checkout from './pages/Checkout';
-import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthContext";
 
 function App() {
-  return <div className="app">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/auth" element={<Auth />}/>
-      <Route path="/checkout" element={<Checkout />}/>
-    </Routes>
-  </div>;
+  return (
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+
+  );
 }
 
 export default App;
